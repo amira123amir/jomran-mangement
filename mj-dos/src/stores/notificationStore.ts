08a7f567b0f19b42427c4ce4d6cf6ee65a4829fa
amiraa123amir <alasaad1234alasaad@gmail.com>
2026-07-20 10:20:11 +0300

    third commit

--- mj-dos/src/stores/notificationStore.ts
+++
@@ -1,5 +0,0 @@
-import { useOrderStore } from './orderStore';
-
-export function getUnreadCount(persona: string): number {
-  return useOrderStore.getState().getNotificationsFor(persona).filter((n) => !n.read).length;
-}
